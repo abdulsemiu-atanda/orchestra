2e1a34b3554abf7db3471fcc3a9115a77925a923
Abdulsemiu Atanda <semiu@live.com>
2026-08-09 00:24:04 -0400

    wire up cli commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-  println!("Hello, world!");
+mod commands;
+mod runners;
+mod services;
+mod errors;
+
+use commands::build_cli;
+use runners::run_config_command;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+  let matches = build_cli().get_matches();
+
+  match matches
+    .subcommand()
+    .expect("Clap should prevent calling this without a subcommand.")
+  {
+    ("config", matches) => run_config_command(matches).await?,
+    _ => unreachable!("The cli parser should prevent reaching here"),
+  };
+  Ok(())
 }
